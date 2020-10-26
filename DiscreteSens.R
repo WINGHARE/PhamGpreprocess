02d9d6019739a4d8dd1ffe1054de77fb6256aaf4
Junyi CHEN <winghare@gmail.com>
2020-10-26 16:28:16 +0800

    Edit the binary process

--- DiscreteSens.R
+++ DiscreteSens.R
@@ -5,4 +5,14 @@ library(PharmacoGx)
 source("callingWaterFall.R")
 
 df.sens<- read.csv("data/GDSC2_label_9drugs.csv")
-reult <- callingWaterfall(df.sens$Cisplatin,type=c("AUC"),intermediate.fold = c(0),plot=TRUE)
+
+for(cd in colnames(df.sens)[2:9]){
+  
+  result <- callingWaterfall(df.sens[,c(cd)],type=c("AUC"),intermediate.fold = c(0),plot=TRUE)
+  
+  df.sens[,paste(cd,"binary",sep=".")] <-result
+}
+
+df.sens[,9:18]
+
+write.csv(df.sens[,9:18],"data/GDSC2_label_9drugs_binary.csv")
